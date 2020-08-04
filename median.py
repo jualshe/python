81dfa55fe51226e78e0e0b999279c9411da31863
jualshe <shevchenko.j@gmail.com>
2020-08-04 00:26:58 -0700

    Update median.py

---
+++ median.py
@@ -0,0 +1,22 @@
+def bigger(a,b):
+	if a > b:
+		return a
+	else:
+		return b
+
+def biggest (a,b,c):
+	return bigger(a, bigger(b,c))		
+
+def median (a,b,c):
+	big = biggest (a,b,c)
+	if big == a:
+		return bigger (b,c)
+	if big ==b:
+		return bigger (a,c)
+	else: 
+		return bigger(a,b)
+
+
+	7 8 9 
+	a b c 
+	if a > b
