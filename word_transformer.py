26bb05693a469831180dbec787b82fc42a33e3f9
jualshe <shevchenko.j@gmail.com>
2020-08-06 00:12:11 -0700

    Update word_transformer.py

---
+++ word_transformer.py
@@ -0,0 +1,23 @@
+from random import randint
+
+def random_verb():
+    random_num = randint(0, 1)
+    if random_num == 0:
+        return "run"
+    else:
+        return "kayak"
+        
+def random_noun():
+    random_num = randint(0,1)
+    if random_num == 0:
+        return "sofa"
+    else:
+        return "llama"
+
+def word_transformer(word):
+    if word == "noun":
+        return random_noun()
+    if word == "verb":
+        return random_verb()
+    else: 
+        return word[0]
