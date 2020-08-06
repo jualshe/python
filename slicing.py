e49089a593dc72ea4f432679676d6497ffd0f4f9
jualshe <shevchenko.j@gmail.com>
2020-08-05 22:49:02 -0700

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,10 @@
+sentence = "A NOUN went on a walk."
+substring = sentence[6:]
+
+
+#another task
+
+sentence = "A NOUN went on a walk. It can VERB really fast."
+substring1 = sentence[:2]
+substring2 = sentence[6:30]
+substring3 = sentence[34:]
