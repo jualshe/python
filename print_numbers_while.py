360a6b540bd37328a0d3cc65851cfab7a577ca62
jualshe <shevchenko.j@gmail.com>
2020-08-06 13:02:49 -0700

    Create print_numbers_while.py

---
+++ print_numbers_while.py
@@ -0,0 +1,24 @@
+def print_numbers (a):
+	i = 1
+	while i <= a:
+		print i
+		i = i + 1
+print print_numbers(3)
+
+
+
+def print_numbers (n):
+	i = 0
+	while i < n:
+		i = i + 1
+		print i
+print print_numbers(3)
+
+
+def print_numbers (n):
+	i = 1
+	while True:
+		if i > n:
+			break
+	print i
+	i = i + 1
