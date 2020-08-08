08c742a673eab50ff404befb3fe1334ffcd23abb
jualshe <shevchenko.j@gmail.com>
2020-08-08 15:37:16 -0700

    Create nested_lists.py

---
+++ nested_lists.py
@@ -0,0 +1,9 @@
+nested_list = [['HTML', 'Hypertext Markup Language forms the structure of webpages'],
+               ['CSS' , 'Cascading Style Sheets give pages style'],
+               ['Python', 'Python is a programming language'],
+               ['Lists', 'Lists are a data structure that let you organize information']]
+
+first_concept = nested_list[0]
+first_title = first_concept[0]
+first_description = first_concept[1]
+print (nested_list[0][0])
