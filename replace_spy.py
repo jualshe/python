7667f88be4a46c7cc1c7b810a525821281db7ed1
jualshe <shevchenko.j@gmail.com>
2020-08-10 00:11:30 -0700

    Update replace_spy.py

--- replace_spy.py
+++ replace_spy.py
@@ -1,5 +1,10 @@
-spy=[0,0,7]
+def replace_spy(p):
+	p[2]=p[2]+1
+	#no return statement here - we dont' need to return new value -
+	#we've modified the value of the list that was passed in
 
-def replace_spy(spy):
-	new_spy=spy[2]=spy[2]+1
-	return new_spy
+
+spy=[0,0,7]
+replace_spy(spy)
+print spy 
+#should be [0,0,8]
