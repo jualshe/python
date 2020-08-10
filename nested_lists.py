0e31df5335e60d9a4d54b21f69a519cb9165e20a
jualshe <shevchenko.j@gmail.com>
2020-08-09 22:42:49 -0700

    Update nested_lists.py

--- nested_lists.py
+++ nested_lists.py
@@ -7,3 +7,11 @@ first_concept = nested_list[0]
 first_title = first_concept[0]
 first_description = first_concept[1]
 print (nested_list[0][0])
+
+#             Name    Capital  Populations (millions)
+countries = [['China','Beijing',1350],
+             ['India','Delhi',1210],
+             ['Romania','Bucharest',21],
+             ['United States','Washington',307]]
+india_capital=countries[1][1]
+print india_capital
