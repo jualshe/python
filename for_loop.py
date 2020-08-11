beb24c8e71fd0b70743f5be0a665f8dc6a816b11
jualshe <shevchenko.j@gmail.com>
2020-08-11 00:34:27 -0700

    Update for_loop.py

--- for_loop.py
+++ for_loop.py
@@ -8,3 +8,23 @@ def print_all_elements(p):
 mylist=[1,2,[3,4]]
 
 print print_all_elements(mylist)
+
+
+#quiz1
+print "EXAMPLE 1: We can use for loops to go through a list of strings"
+example_list_1 = ['a', 'b', 'c', 'd']
+for thing in example_list_1:
+    print thing
+
+#quiz2
+print "EXAMPLE 2: We can use for loops on nested lists too!"
+example_list_2 = [['China', 'Beijing'],
+                  ['USA'  , 'Washington D.C.'],
+                  ['India', 'Delhi']]
+for country_with_capital in example_list_2:
+    country = country_with_capital[0]
+    capital = country_with_capital[1]
+    print capital + ' is the capital of ' + country
+
+
+    
