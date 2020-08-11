9bcb7920759ce73d238fa02776a5dc936537e88e
jualshe <shevchenko.j@gmail.com>
2020-08-11 01:00:35 -0700

    Update lists.py

--- lists.py
+++ lists.py
@@ -2,3 +2,5 @@ list_with_lists = [3, 'colors:', ['red', 'green', 'blue'], 'your favorite?']
 print list_with_lists
 list_in_the_list = list_with_lists[2]
 print list_with_lists
+
+stooges = ["Moe","Larry","Curly"]
