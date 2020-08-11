132296169593503c9ef2bca632ddfcff8badf486
jualshe <shevchenko.j@gmail.com>
2020-08-10 23:26:46 -0700

    Create list_operations.py

---
+++ list_operations.py
@@ -0,0 +1,32 @@
+#append
+
+stooges.append('Shemp')
+
+#<list>+<list>
+
+[0,1]+[2,3] -> [0,1,2,3]
+
+
+#len(<list>)
+len([0,1]) ->2
+len(['a',['b',['c']]]) ->2
+len("ababagalamaga") -> 13
+
+#quiz 
+p = [1,2]
+p.append(3)
+p = p + [4,5]
+print len(p)
+->5
+
+
+#quiz2
+
+p =[1,2]
+q = [3,4]
+p.append(q)
+print len(p)
+
+->3 
+#print p
+#[1, 2, [3, 4]]
