44ae33cfdf394870043e071bea0f7c0088793a7d
jualshe <shevchenko.j@gmail.com>
2020-08-13 23:19:43 -0700

    Update binary_search.py

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -4,9 +4,20 @@
 # guess = list[mid]
 
 def binary_search(list, item):
-	low = o
+	low = 0
 	high = len(list)-1
 
 	while low <= high:
 		mid = (low + high)
 		guess = list[mid]
+		if guess == item:
+			return mid
+		if guess > item:
+			high = mid - 1
+		else:
+			low = mid + 1
+		return None
+
+
+print binary_search([1, 3, 5, 7, 9], 3)
+print binary_search([1, 3, 5, 7, 9], -1)
