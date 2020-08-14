e02985ad41958b1a2203df5fbc3a7b3fa6d91c23
jualshe <shevchenko.j@gmail.com>
2020-08-13 23:14:59 -0700

    Create binary_search.py

---
+++ algorithms/binary_search.py
@@ -0,0 +1,12 @@
+# low = 0
+# high = len(list) - 1
+# mid = (low + high) / 2 
+# guess = list[mid]
+
+def binary_search(list, item):
+	low = o
+	high = len(list)-1
+
+	while low <= high:
+		mid = (low + high)
+		guess = list[mid]
