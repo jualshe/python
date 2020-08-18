dcf48a366ae9059bf5937a3a0317f8a15bbaa571
jualshe <shevchenko.j@gmail.com>
2020-08-17 23:38:15 -0700

    Update none.py

--- none.py
+++ none.py
@@ -3,3 +3,14 @@ print(myval)
 
 if myval == None:
 	print ("myval is empty")
+
+#converter
+
+age = '10.5'
+converted_age = float(age)
+
+age = '10'
+converted_age = int(age)
+
+age = 10
+converted_age = str(age)
