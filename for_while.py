efe030a59321ddfdba4036a2a7f8b7e3e096fcc4
jualshe <shevchenko.j@gmail.com>
2020-08-18 23:58:28 -0700

    updated for while loops

--- for_while.py
+++ for_while.py
@@ -10,7 +10,7 @@ for x in range(0, 5):
 
 
 
-
+#______________________
 >> x = 0
 >>> print('hi there %s' % x) 
 hi there 0
@@ -26,3 +26,24 @@ hi there 3
 >>> x = 4
 >>> print('hi there %s' % x) 
 hi there 4
+
+
+#______________________
+wizard_list = ['pew1', 'pew2', 'pew3', 'pew4', 'ololo', 'looking']
+    for i in wizard_list:
+    	print(i)
+
+ #without for
+wizard_list = ['pew1', 'pew2', 'pew3', 'pew4', 'ololo', 'looking']
+print(wizard_list[0])
+pew1
+print(wizard_list[1])
+pew2
+print(wizard_list[2])
+pew3
+print(wizard_list[3])
+pew4
+print(wizard_list[4])
+ololo
+print(wizard_list[5])
+looking
