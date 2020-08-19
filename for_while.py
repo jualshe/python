156dffe56c94c89a78c0b844cbcabef1f37d3cc7
jualshe <shevchenko.j@gmail.com>
2020-08-18 23:43:15 -0700

    Update for_while.py

---
+++ for_while.py
@@ -0,0 +1,28 @@
+for x in range(0, 5):
+print('hello')
+
+#______________________
+print(list(range(10, 20)))
+
+#______________________
+for x in range(0, 5):
+	print('hi %s count' %x)
+
+
+
+
+>> x = 0
+>>> print('hi there %s' % x) 
+hi there 0
+>>> x = 1
+>>> print('hi there %s' % x) 
+hi there 1
+>>> x = 2
+>>> print('hi there %s' % x) 
+hi there 2
+>>> x = 3
+>>> print('hi there %s' % x) 
+hi there 3
+>>> x = 4
+>>> print('hi there %s' % x) 
+hi there 4
