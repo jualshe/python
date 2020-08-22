b65c4f325fc2acc965954266514268752a4d63eb
jualshe <shevchenko.j@gmail.com>
2020-08-22 10:21:20 -0700

    Update if_then_else.py

--- if_then_else.py
+++ if_then_else.py
@@ -5,4 +5,19 @@ elif age == 11:
 	print("eleven")
 elif age == 12:
 	print("twelve")	
-	
+else:
+	print("whatever")
+
+
+#several ifs
+if age == 10 or age == 11 or age == 12 or age == 13:
+	print ("10,11,12 or 13")
+else:
+	print("something else")
+
+#OR
+if age >= 10 and age <= 13:
+	print ("10,11,12 or 13")
+else:
+	print ("ololosha")
+ 
