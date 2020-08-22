4a6b1b72ca33097743cf7acc10a967490a303a5f
jualshe <shevchenko.j@gmail.com>
2020-08-22 11:36:32 -0700

    Update while.py

--- while.py
+++ while.py
@@ -20,3 +20,17 @@ for x in range (0,40,2):
 	if x > 33:
 		break
 	print (x)
+
+for i in range (0,40):
+	print (x%2)
+
+
+#_______________
+ingredients = ["carrots","avocado","edamame","ahi tuna","wasabi aioli"]
+for i in range(len(ingredients)):
+    print (str(i)+' ' +ingredients[i])
+    
+
+
+
+
