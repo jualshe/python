5216d4f613acb108a2d180cc21be7d1425b69d05
jualshe <shevchenko.j@gmail.com>
2020-08-23 23:51:59 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -4,3 +4,15 @@ def testfunc(myname):
 
 #call 
 #testfunc('julia')
+
+def testfunc(fname, lname):
+              print('hi there, %s %s' % (fname, lname))
+
+#call 
+#testfunc('julia','smith')
+
+
+def savings(pocket_money, paper_route, spending): 
+	return pocket_money + paper_route - spending
+
+print savings (10,10,5)
