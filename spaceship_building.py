160b98a3238cc1367cf09c187e7b83034283ee79
jualshe <shevchenko.j@gmail.com>
2020-08-24 00:17:38 -0700

    Update spaceship_building.py

--- spaceship_building.py
+++ spaceship_building.py
@@ -1,1 +1,11 @@
 def spaceship_building(cans):
+	total_cans = 0 
+	for week in range(1,553):
+		total_cans = total_cans+cans
+		if total_cans >500:
+			break
+		print('week %s, cans: %s' % (week, total_cans))
+
+
+
+spaceship_building(2)
