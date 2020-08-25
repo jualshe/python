1bf8282ee4a8aa6cbc8d5d24d47caa1cff5544ec
jualshe <shevchenko.j@gmail.com>
2020-08-24 23:43:48 -0700

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,2 +1,16 @@
 import time
 print(time.asctime())
+
+
+
+import sys
+print(sys.stdin.readline())
+
+
+def silly_age_joke(age):
+	print("how old are you?")
+	age = int(sys.stdin.readline())
+	if age >= 10 and age <= 13:
+		print("good age!")
+	else: 
+		print("other")
