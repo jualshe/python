afc9ec4badf3bc4b24b5680a4efb5f11cc0f903b
jualshe <shevchenko.j@gmail.com>
2020-08-26 00:03:51 -0700

    Update weightOnMoon.py

--- weightOnMoon.py
+++ weightOnMoon.py
@@ -1,6 +1,4 @@
 moonWeight = 9.075
 for i in range (2000,2015):
-	moonWeight = moonWeight + 1
-return moonWeight
-    
-print (moonWeight) 	
+	moonWeight = moonWeight + 1    
+	print (i, "year -", moonWeight, "kilos")
