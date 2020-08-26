b26c1580fae032b0fcea2a82b8ab40112ba5126a
jualshe <shevchenko.j@gmail.com>
2020-08-26 00:50:06 -0700

    Update weightOnMoon.py

--- weightOnMoon.py
+++ weightOnMoon.py
@@ -1,7 +1,14 @@
-def moonWeight(initialWeight, increaseNumber):
+def moonWeight(initialWeight, increaseNumber, age):
+	print("what is your weight?")
+	initialWeight = int(sys.stdin.readline())
+	print("what is your increaseNumber ?")
+	increaseNumber = int(sys.stdin.readline())
+	print("how old are you?")
+	age = int(sys.stdin.readline())
 	moonWeight = initialWeight * increaseNumber
 	for i in range (2020,2035):
 		moonWeight = moonWeight + 1    
-		print (i, "year -", moonWeight, "kilos")
+		age = age+1
+		print (i, "year -", moonWeight, "kilos", age)
 
-#print moonWeight(55,0.25)
+#print moonWeight(55,0.25, 33)
