f95746e5f902724dae5023a8e7d36522385ff5f9
jualshe <shevchenko.j@gmail.com>
2020-08-29 00:08:16 -0700

    update def nextDay(year, month, day):

--- days_between_dates.py
+++ days_between_dates.py
@@ -1,29 +1,9 @@
-daysOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-def isLeapYear (year):
-	#return true or false
-#____________________________________________________________
-
-def nextDay(year, month, day): #if every month has 30 days
-	if day < 30:
-		return year, month, day+1
-	else:
-		if month < 12:
-			return year, mont+1, 1
-		else:
-			return year+1,1,1
-#____________________________________________________________
-def daysBetweenDates(y1,m1,d1,y2,m2,d2):
+def daysBetweenDates(year1,month1,day1,year2,month2,day2):
 	days = 0
-
-	if y1<y2:
-		y=(y2-y1)*365
-	if m1<m2:
-		m=(m2-m1)*30
-	while d1<d2:
-		d=(d2-d1)*1
-	days = y+m+d
-	return days
+	while dateIsBefore(year1,month1,day1,year2,month2,day2):
+		year1,month1,day1 = nextDay(year1,month1,day1)
+		days+=1
+	return days	
 
 print daysBetweenDates(2000,10,30,2010,20,20)
 
@@ -41,9 +21,55 @@ def dateIsBefore(y1,m1,d1,y2,m2,d2):
 
 print dateIsBefore(2000,10,30,2010,20,20)
 
+
 #____________________________________________________________
-Algorithm Pseudocode
-days = 0 # of days in month1 - day1
+#if every month has 30 days
+def nextDay(year, month, day): 
+	if day < 30:
+		return year, month, day+1
+	else:
+		if month < 12:
+			return year, month+1, 1
+		else:
+			return year+1,1,1
+
+print nextDay (2000,10,10)
+
+
+# daysOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#____________________________________________________________
+# def daysBetweenDates(y1,m1,d1,y2,m2,d2):
+# 	days = 0
+
+# 	if y1<y2:
+# 		y=(y2-y1)*365
+# 	if m1<m2:
+# 		m=(m2-m1)*30
+# 	while d1<d2:
+# 		d=(d2-d1)*1
+# 	days = y+m+d
+# 	return days
+
+# print daysBetweenDates(2000,10,30,2010,20,20)
+#____________________________________________________________
+#pseudocode
+days = 0
+while date1 is before date2
+	date1 = date after date1 #next date
+	days+=1
+return days
+
+def dateIsBefore(year1,month1,day1,year2,month2,day2)
+
+#____________________________________________________________
+
+def isLeapYear (year):
+	#return true or false
+
+
+#____________________________________________________________
+#Algorithm Pseudocode
+days =  # of days in month1 - day1
 month1 += 1
 while month1 < month2:
     days += # of days in month1
@@ -52,7 +78,7 @@ days += day2
 while year1 < year2:
     days += days in year1
 
-#simplier
+#simplier - Different Approach
 days = 0
 while date1 is before date2:
     date1 = day after date1
