4a12fb975e1e1063a6d32d630941241911aff4d9
jualshe <shevchenko.j@gmail.com>
2020-08-31 06:37:36 -0700

    Update days_between_dates.py

--- days_between_dates.py
+++ days_between_dates.py
@@ -1,7 +1,8 @@
 #that always returns 30
 def daysInMonth(year, month):
-	if month == 1 or month == 3 or month == 5 or 
-
+	if month == 1 or month == 3 or month == 5 or month ==7 \
+		or month == 8 or month == 10 or month ==12:
+			return 31
 	return 30
 
 
