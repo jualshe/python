f03297c3dbc48859e5254d049fe88abeee0cd535
jualshe <shevchenko.j@gmail.com>
2020-09-02 13:13:20 -0700

    Update days_between_dates.py

--- days_between_dates.py
+++ days_between_dates.py
@@ -1,19 +1,29 @@
-#that always returns 30
 def daysInMonth(year, month):
 	if month == 1 or month == 3 or month == 5 or month ==7 \
 		or month == 8 or month == 10 or month ==12:
 			return 31
+	else:
+		if month == 2:
+				if isLeapYear(year):
+					return 29
+				else:
+					return 28
+		else:
+			return 30
 	return 30
 
 
 def test():
 	assert daysBetweenDates(2013, 1, 1, 2013, 1, 1) == 0
 	assert daysBetweenDates(2013, 1, 1, 2013, 1, 2) == 0
-	assert nextDay(2013,1,1)==(2013,1,2)
-	assert nextDay(2013,4,30)==(2013,5,1)
-	assert nextDay(2012,12,31)==(2013,1,1)
-	assert nextDay(2013,2,28)==(2013,3,1)
-	assert nextDay(2013,9,30)==(2013,10,1)
+	assert nextDay(2013,1,1) == (2013,1,2)
+	assert nextDay(2013,4,30) == (2013,5,1)
+	assert nextDay(2012,12,31) == (2013,1,1)
+	assert nextDay(2013,2,28) == (2013,3,1)
+	assert nextDay(2013,9,30) == (2013,10,1)
+	assert nextDay(2012, 2, 28) == (2012, 2, 29)
+	assert daysBetweenDates(2012, 1, 1, 2013, 1, 1)
+	assert daysBetweenDates(2013, 1, 1, 2014, 1, 1) == 365
 	print "tests are passed!"
 
 
@@ -51,12 +61,12 @@ print dateIsBefore(2000,10,30,2010,20,20)
 #    """Simple version: assume every month has 30 days"""
 def nextDay(year, month, day): 
 	if day < daysInMonth(year, month):
-		return year, month, day+1
+		return year, month, day + 1
 	else:
 		if month < 12:
-			return year, month+1, 1
+			return year, month + 1, 1
 		else:
-			return year+1,1,1
+			return year + 1,1,1
 
 print nextDay (2000,10,10)
 
