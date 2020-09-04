6b67428f3d02b20cd77e196e398a33cb6a75cd5a
jualshe <shevchenko.j@gmail.com>
2020-09-04 10:53:37 -0700

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,2 +1,20 @@
 class Things:
               pass
+
+class Inanimate(Things):
+              pass
+
+class Animate(Things):
+              pass
+
+class Sidewalks(Inanimate):
+              pass
+
+class Animals(Animate):
+              pass
+
+class Mammals(Animals):
+              pass
+
+class Giraffes(Mammals):
+              pass
