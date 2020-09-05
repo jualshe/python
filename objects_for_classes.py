57c6d23211fd2cfa215cee99737118f28de371ac
jualshe <shevchenko.j@gmail.com>
2020-09-05 09:43:47 -0700

    Update objects_for_classes.py

--- objects_for_classes.py
+++ objects_for_classes.py
@@ -1,1 +1,8 @@
 reginald = Giraffes()
+
+def this_is_a_normal_function(): print('regular function')
+
+class ThisIsMySillyClass:
+              def this_is_a_class_function():
+print('function of the class') def this_is_also_a_class_function():
+                    print('it\'s also function of the class?')
