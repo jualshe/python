1b0878c54f7cbd89033a451566dec638ea639eb7
jualshe <shevchenko.j@gmail.com>
2020-09-23 14:51:01 -0700

    Update objects_for_classes.py

--- objects_for_classes.py
+++ objects_for_classes.py
@@ -53,6 +53,11 @@ reginald.move()
 harold.eat_leaves_from_trees()
 
 
-mport turtle
+import turtle
 avery = turtle.Pen() 
 kate = turtle.Pen()
+
+
+avery.forward(50)
+avery.right(90)
+avery.forward(20)
