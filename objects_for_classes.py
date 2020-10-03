3974d9c7575558c8eeaf73724d19decb0fd11752
jualshe <shevchenko.j@gmail.com>
2020-10-02 18:03:25 -0700

    Update objects_for_classes.py

--- objects_for_classes.py
+++ objects_for_classes.py
@@ -103,4 +103,10 @@ reginald.dance_a_jig()
 
 class Giraffes:
               def __init__(self, spots):
-self.giraffe_spots = spots
+self.giraffe_spots = spotsgi 
+
+
+ozwald = Giraffes(100)
+gertrude = Giraffes(150)
+print(ozwald.giraffe_spots)
+print(gertrude.giraffe_spots)
