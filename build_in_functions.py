4d5010fb9119b63980732e549dc094cc8d45119c
jualshe <shevchenko.j@gmail.com>
2020-10-05 23:07:57 -0700

    Update build_in_functions.py

--- build_in_functions.py
+++ build_in_functions.py
@@ -8,12 +8,20 @@ if abs(steps) > 0:
 
 #bool
 print(bool(0))
+>>>False
 print(bool(1))
 print(bool(1123.23))
 print(bool(-500))
 
 print(bool(None))
+>>>False
 print(bool('a'))
 print(bool(' '))
 print(bool('What do you call bears with no ears? B.'))
 
+my_silly_list = []
+print(bool(my_silly_list))
+>>>False
+my_silly_list = ['s', 'i', 'l', 'l', 'y']
+print(bool(my_silly_list))
+>>>True
