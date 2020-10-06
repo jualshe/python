442e6eaa1f2e1b73355c741d0df81d3e72d079e4
jualshe <shevchenko.j@gmail.com>
2020-10-05 23:01:16 -0700

    Update build_in_functions.py

--- build_in_functions.py
+++ build_in_functions.py
@@ -1,2 +1,19 @@
+#abs
 print(abs(10)) 
 print(abs(-10))
+
+steps = -3
+if abs(steps) > 0:
+	print('Character moves')
+
+#bool
+print(bool(0))
+print(bool(1))
+print(bool(1123.23))
+print(bool(-500))
+
+print(bool(None))
+print(bool('a'))
+print(bool(' '))
+print(bool('What do you call bears with no ears? B.'))
+
