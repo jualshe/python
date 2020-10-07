2c21e2cc39ed9fb0bcaa2e75dba9b381d0146af9
jualshe <shevchenko.j@gmail.com>
2020-10-06 23:52:38 -0700

    Update build_in_functions.py

--- build_in_functions.py
+++ build_in_functions.py
@@ -25,3 +25,18 @@ print(bool(my_silly_list))
 my_silly_list = ['s', 'i', 'l', 'l', 'y']
 print(bool(my_silly_list))
 >>>True
+
+year = input('Year of birth: ')
+if not bool(year.rstrip()):
+              print('enter the year')
+
+#dir
+dir(['this is', 'short', 'list'])
+['__add__', '__class__', '__contains__', '__delattr__', '__delitem__', '__doc__', '__eq__',
+'__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__',
+'__imul__', '__init__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__',
+'__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', 
+'__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 
+'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+
+dir(1)
