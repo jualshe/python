0951efad2beeb56c0c02f17c59b5f6192c4b6a0c
jualshe <shevchenko.j@gmail.com>
2020-10-14 23:33:58 -0700

    Update useful_modules.py

--- useful_modules.py
+++ useful_modules.py
@@ -1,2 +1,9 @@
 import turtle
 t = turtle.Pen()
+
+
+lass Animal:
+              def __init__(self, species, number_of_legs, color):
+					self.species = species 
+					self.number_of_legs = number_of_legs 
+					self.color = color
