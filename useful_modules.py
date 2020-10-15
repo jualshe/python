9de66284c3f137b693223f9a46562c3eba28b4af
jualshe <shevchenko.j@gmail.com>
2020-10-14 23:35:11 -0700

    Update useful_modules.py

--- useful_modules.py
+++ useful_modules.py
@@ -2,8 +2,17 @@ import turtle
 t = turtle.Pen()
 
 
-lass Animal:
+class Animal:
               def __init__(self, species, number_of_legs, color):
 					self.species = species 
 					self.number_of_legs = number_of_legs 
 					self.color = color
+
+harry = Animal('gippogriph', 6, 'pink')
+
+
+import copy
+harry = Animal('гиппогриф', 6, 'розовый')
+harriet = copy.copy(harry)
+print(harry.species)
+print(harriet.species)
