0e4220451b787933a1df34b6e7674227d48f01dc
jualshe <shevchenko.j@gmail.com>
2020-10-16 23:25:58 -0700

    update useful_modules.py

--- useful_modules.py
+++ useful_modules.py
@@ -12,7 +12,19 @@ harry = Animal('gippogriph', 6, 'pink')
 
 
 import copy
-harry = Animal('гиппогриф', 6, 'розовый')
+harry = Animal('gippogriph', 6, 'pink')
 harriet = copy.copy(harry)
 print(harry.species)
 print(harriet.species)
+
+
+harry = Animal('gippogriph', 6, 'pink')
+carrie = Animal('himera', 4, 'in green pees') >>> billy = Animal('bogl', 0, 'patterned')
+my_animals = [harry, carrie, billy]
+more_animals = copy.copy(my_animals)
+print(more_animals[0].species)
+
+gippogriph
+
+print(more_animals[1].species)
+химера
