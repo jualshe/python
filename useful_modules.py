3f2ca852c83c3d9c653b1d64e635573f0c517400
jualshe <shevchenko.j@gmail.com>
2020-10-25 23:32:58 -0700

    Update useful_modules.py

--- useful_modules.py
+++ useful_modules.py
@@ -176,6 +176,12 @@ game_data = {
 'backpack' : ['rope', 'hummer', 'apple'],
 'money' : 158.50
 }
-save_file = open('save.dat', 'wb')
+
+save_file = open('/Users/julia/Documents/GitHub/python/save.dat', 'wb')
 pickle.dump(game_data, save_file)
 save_file.close()
+
+load_file = open('/Users/julia/Documents/GitHub/python/save.dat', 'rb')
+loaded_game_data = pickle.load(load_file)
+load_file.close()
+print(loaded_game_data)
