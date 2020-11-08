73e630e6ae4d6535ebb50b926f821f168e7a926f
jualshe <shevchenko.j@gmail.com>
2020-11-07 23:17:41 -0800

    Update turtle12.py

--- turtle12.py
+++ turtle12.py
@@ -8,3 +8,21 @@ def mycircle(red, green, blue):
 	t.begin_fill() 
 	t.circle(50) 
 	t.end_fill()
+
+mycircle(0, 1, 0)
+mycircle(0, 0.5, 0)
+
+mycircle(1, 0, 0)
+mycircle(0.5, 0, 0)
+mycircle(0, 0, 1)
+mycircle(0, 0, 0.5)
+
+mycircle(0.9, 0.75, 0)
+mycircle(1, 0.7, 0.75)
+
+mycircle(1, 0.5, 0)
+mycircle(0.9, 0.5, 0.15)
+
+mycircle(0, 0, 0)
+
+mycircle(1, 1, 1)
