20bbe54909a90c6bffdc14dcf598b5299386b995
jualshe <shevchenko.j@gmail.com>
2020-11-09 23:56:02 -0800

    Update turtle12.py

--- turtle12.py
+++ turtle12.py
@@ -55,3 +55,16 @@ def mysquare(size, filled):
 		t.left(90) 
 	if filled == True: 
 		t.end_fill()
+
+
+mysquare(50, True)
+
+mysquare(150, False)
+
+
+for x in range(1, 19): 
+	t.forward(100) 
+	if x % 2 == 0: 
+		t.left(175) 
+	else:
+		t.left(225)
