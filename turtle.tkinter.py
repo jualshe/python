6a75cd6680c8a2cc7a72ca5d03ded2d6231861bc
jualshe <shevchenko.j@gmail.com>
2020-11-13 00:36:32 -0800

    Update turtle.tkinter.py

--- turtle.tkinter.py
+++ turtle.tkinter.py
@@ -3,12 +3,19 @@ tk = Tk()
 btn = Button(tk, text="press me") 
 btn.pack()
 
-import turtle
-t = turtle.Pen()
 
-from turtle import * 
-t = Pen()
+# import turtle
+# t = turtle.Pen()
+#or
+# from turtle import * 
+# t = Pen()
 
 
  def hello():
  	print('helloooo')
+
+
+from tkinter import *
+tk = Tk()
+btn = Button(tk, text="press me", command=hello)
+btn.pack()
