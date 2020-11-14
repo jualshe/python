4f402622a5e6c69672b69b0dfc7a6732cb2026c2
jualshe <shevchenko.j@gmail.com>
2020-11-14 02:13:06 -0800

    Update tkinter_canvas.py

--- tkinter_canvas.py
+++ tkinter_canvas.py
@@ -4,4 +4,19 @@ canvas = Canvas(tk, width=500, height=500)
 canvas.pack()
 mainloop()
 
+#drawing lines
+from tkinter import *
+tk = Tk()
+canvas = Canvas(tk, width=500, height=500) 
+canvas.pack()
+canvas.create_line(0, 0, 500, 500)
+1
+mainloop()
 
+import turtle
+turtle.setup(width=500, height=500) 
+t = turtle.Pen()
+t.up()
+t.goto(-250, 250)
+t.down()
+t.goto(500, -500)
