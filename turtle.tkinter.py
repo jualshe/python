5a85b49088462e2e0d0ff360be31de480e74edaa
jualshe <shevchenko.j@gmail.com>
2020-11-13 16:48:59 -0800

    updating turtle.tkinter.py

--- turtle.tkinter.py
+++ turtle.tkinter.py
@@ -19,3 +19,18 @@ from tkinter import *
 tk = Tk()
 btn = Button(tk, text="press me", command=hello)
 btn.pack()
+
+
+from tkinter import * ttk
+ttk.Button(text="Login").pack()
+
+
+import tkinter
+tkinter.Button(None, text='button').pack()
+tkinter.mainloop()
+
+#latest working version
+from tkinter import *
+tk = Tk()
+btn = Button(tk, text="press me") 
+btn.mainloop()
