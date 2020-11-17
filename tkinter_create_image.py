b17eead0a5ec63147d013fe9459eedb8d07f3e11
jualshe <shevchenko.j@gmail.com>
2020-11-17 08:56:52 -0800

    Update tkinter_create_image.py

--- tkinter_create_image.py
+++ tkinter_create_image.py
@@ -1,5 +1,7 @@
 from tkinter import *
 tk = Tk()
-canvas = Canvas(tk, width=400, height=400) canvas.pack()
-my_image = PhotoImage(file='[path]\\dog.gif') canvas.create_image(0, 0, anchor=NW, image=my_image)
+canvas = Canvas(tk, width=400, height=400) 
+canvas.pack()
+my_image = PhotoImage(file='[path]\\dog.gif') 
+canvas.create_image(0, 0, anchor=NW, image=my_image)
 
