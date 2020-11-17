595465a6ae6d90d91d94df7504c10ca8d9a69cfd
jualshe <shevchenko.j@gmail.com>
2020-11-16 23:58:37 -0800

    Update tkinter_canvas.py

--- tkinter_canvas.py
+++ tkinter_canvas.py
@@ -66,4 +66,3 @@ canvas.create_arc(10, 80, 200, 160, extent=90, style=ARC)
 canvas.create_arc(10, 160, 200, 240, extent=135, style=ARC)
 canvas.create_arc(10, 240, 200, 320, extent=180, style=ARC)
 canvas.create_arc(10, 320, 200, 400, extent=359, style=ARC)
-
