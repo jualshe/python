5e959c77d5d30239d475349e423294ee074ad033
jualshe <shevchenko.j@gmail.com>
2020-11-16 23:51:30 -0800

    update tkinter_canvas.py

--- tkinter_canvas.py
+++ tkinter_canvas.py
@@ -34,3 +34,21 @@ tk = Tk()
 canvas = Canvas(tk, width=400, height=400)
 canvas.pack()
 canvas.create_rectangle(10, 10, 300, 50)
+
+#to convert 15 from decimal to hexadecimal
+>>>print('%x' % 15)
+f
+>>> print('%02x' % 15)
+0f
+
+from tkinter import * 
+colorchooser.askcolor()
+
+>>> colorchooser.askcolor()
+((235.91796875, 86.3359375, 153.59765625), '#eb5699')
+
+c = colorchooser.askcolor()
+random_rectangle(400, 400, c[1])
+
+#arc drawing
+canvas.create_arc(10, 10, 200, 100, extent=180, style=ARC)
