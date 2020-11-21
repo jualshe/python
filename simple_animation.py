3eab5c8e957fd2374512ddee1b991263b8a56b3d
jualshe <shevchenko.j@gmail.com>
2020-11-21 00:16:46 -0800

    Update simple_animation.py

--- simple_animation.py
+++ simple_animation.py
@@ -12,7 +12,8 @@ for x in range(0, 60):
 import time
 from tkinter import *
 tk = Tk()
-canvas = Canvas(tk, width=400, height=400) canvas.pack()
+canvas = Canvas(tk, width=400, height=400) 
+canvas.pack()
 canvas.create_polygon(10, 10, 10, 60, 50, 35) 
 for x in range(0, 60):
 	canvas.move(1, 5, 5) 
