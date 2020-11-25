9776cb543abccd3a0528ed4b2c9d88de6f032de1
jualshe <shevchenko.j@gmail.com>
2020-11-24 23:32:53 -0800

    Update tkinter_random_triangle.py

--- tkinter_random_triangle.py
+++ tkinter_random_triangle.py
@@ -10,4 +10,13 @@ def random_triangle(width, height, fill_color):
     y2 = random.randrange(y1 + random.randrange(height))
     x3 = random.randrange(x2 + random.randrange(width))
     y3 = random.randrange(y2 + random.randrange(height))
-    canvas.create_rectangle(x1, y1, x2, y2, x3, y3, fill=fill_color)
+    canvas.create_polygon(x1, y1, x2, y2, x3, y3, fill=fill_color)
+
+#run module
+random_triangle(400, 400, '#ffd800')
+random_triangle(400, 400, 'green')
+
+
+
+for x in range(0, 5):
+	random_triangle(400, 400, 'cyan')
