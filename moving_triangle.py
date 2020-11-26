929f120ffb673ccbdb3ae97d8f3d347bde20ff06
jualshe <shevchenko.j@gmail.com>
2020-11-26 00:30:49 -0800

    update sleep time for faster moving

--- moving_triangle.py
+++ moving_triangle.py
@@ -1,15 +1,26 @@
+import time
 from tkinter import *
 tk = Tk()
 canvas = Canvas(tk, width=400, height=400) 
 canvas.pack()
 canvas.create_polygon(10, 10, 10, 60, 50, 35) 
 
-for x in a range (0, 60):
+for x in range(0, 60):
 	canvas.move(1,5,0)
 	tk.update()
-	time.sleep(0.05)
+	time.sleep(0.005)
 
 for x in range(0, 60): 
-	canvas.move(1, -5, -5)
+	canvas.move(1, 0, 5)
 	tk.update() 
-	time.sleep(0.05)	
+	time.sleep(0.005)
+
+for x in range(0, 60):
+	canvas.move(1,-5,0)
+	tk.update()
+	time.sleep(0.005)
+
+for x in range(0, 60): 
+	canvas.move(1, 0, -5)
+	tk.update() 
+	time.sleep(0.005)
