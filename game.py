47a1f6b89f17e2b4e7781d9706bc8962ce6ca181
jualshe <shevchenko.j@gmail.com>
2020-11-28 23:48:25 -0800

    Update game.py

--- game.py
+++ game.py
@@ -12,6 +12,12 @@ class Ball:
 		self.canvas_height = self.canvas.winfo_height()
 	def draw(self): 
 		self.canvas.move(self.id, 0, -1)
+		self.canvas.move(self.id, self.x, self.y) 
+		pos = self.canvas.coords(self.id)
+		if pos[1] <= 0:
+			self.y = 1
+		if pos[3] >= self.canvas_height:
+			self.y = -1
 tk = Tk()
 tk.title("Игра")
 tk.resizable(0, 0)
