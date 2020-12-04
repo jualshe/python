83d4e91360daa4f55bddf25c22fe20b2ebb4bd70
jualshe <shevchenko.j@gmail.com>
2020-12-03 23:59:35 -0800

    Update game.py

--- game.py
+++ game.py
@@ -26,6 +26,14 @@ class Ball:
 		if pos[2] >= self.canvas_width:
 			self.x = -3
 
+class Paddle:
+          def __init__(self, canvas, color):
+self.canvas = canvas
+self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+self.canvas.move(self.id, 200, 300)
+          def draw(self):
+              pass
+
 tk = Tk()
 tk.title("Игра")
 tk.resizable(0, 0)
