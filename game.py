05dd6bda0342b28436ac28bfe792afbce08445ed
jualshe <shevchenko.j@gmail.com>
2020-12-05 23:57:09 -0800

    Update game.py

--- game.py
+++ game.py
@@ -35,16 +35,18 @@ self.canvas.move(self.id, 200, 300)
               pass
 
 tk = Tk()
-tk.title("Игра")
+tk.title("Game")
 tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack() tk.update()
 
+paddle = Paddle(canvas, 'blue')
 ball = Ball(canvas, 'red')
 
 while 1: 
 	ball.draw()
+	paddle.draw()
 	tk.update_idletasks() 
 	tk.update() 
 	time.sleep(0.01)
