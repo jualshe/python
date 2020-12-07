b1d94ca316e2a6bdf68fd3cec3428046f40282a3
jualshe <shevchenko.j@gmail.com>
2020-12-06 23:46:56 -0800

    Update game.py

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ import time
 class Ball:
 	def __init__(self, canvas, color):
 		self.canvas = canvas
+		self.paddle = paddle
 		self.id = canvas.create_oval(10, 10, 25, 25, fill=color) 
 		self.canvas.move(self.id, 245, 100)
 		starts = [-3, -2, -1, 1, 2, 3]
