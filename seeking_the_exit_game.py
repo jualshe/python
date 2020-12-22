1faeb5f713daabe8c9c5c9a710ece70783bff048
jualshe <shevchenko.j@gmail.com>
2020-12-21 23:59:10 -0800

    Update seeking_the_exit_game.py

--- seeking_the_exit_game.py
+++ seeking_the_exit_game.py
@@ -1,2 +1,12 @@
 #install gimp.org
 #background Layer ▶ Transparency ▶ Add Alpha Channel 
+
+#create graphics for the game
+
+#human
+
+#3 sizes of a platform
+
+#doors : open and close
+
+#background (something fun)
