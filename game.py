97f6847370ef47b983b682a9cd176d4fa336e842
jualshe <shevchenko.j@gmail.com>
2020-12-25 01:13:44 -0800

    Update game.py

--- game.py
+++ game.py
@@ -2,15 +2,6 @@ from tkinter import *
 import random
 import time
 
-#Delay before the start of the game - time.sleep(2)
-
-import tkinter as tk
-root = tk.Tk()
-var = tk.IntVar()
-button = tk.Button(root, text="Start Game!!!", command=lambda: var.set(1))
-button.place(relx=.5, rely=.5, anchor="c")
-button.wait_variable(var)
-
 class Ball:
     def __init__(self, canvas, paddle, color):
         self.canvas = canvas
@@ -24,14 +15,12 @@ class Ball:
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
         self.hit_bottom = False
-
     def hit_paddle(self, pos):
         paddle_pos = self.canvas.coords(self.paddle.id)
         if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
             if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
                 return True
             return False
-
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
@@ -45,7 +34,6 @@ class Ball:
             self.x = 3
         if pos[2] >= self.canvas_width:
             self.x = -3
-
 class Paddle:
     def __init__(self, canvas, color):
         self.canvas = canvas
@@ -53,13 +41,18 @@ class Paddle:
         self.canvas.move(self.id, 200, 300)
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
+        self.started = False
         self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
         self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        self.canvas.bind_all('<Button-1>',self.start_game)
         
     def turn_left(self, evt):
         self.x = -2
     def turn_right(self, evt):
         self.x = 2
+
+    def start_game(self, evt):
+        self.started = True
         
     def draw(self):
         self.canvas.move(self.id, self.x, 0)
@@ -68,6 +61,13 @@ class Paddle:
             self.x = 0
         elif pos[2] >= self.canvas_width:
             self.x = 0
+class End:
+    def game_over():
+        tk = Tk()
+        canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+        canvas.pack()
+        canvas.create_text(150, 100, text='game over!!')
+        tk.update()
 
 tk = Tk()
 tk.title("Game")
@@ -81,38 +81,28 @@ paddle = Paddle(canvas, 'blue')
 ball = Ball(canvas,paddle, 'red')
 
 while 1:
-    if ball.hit_bottom == False:
+    if ball.hit_bottom == False and paddle.started == True:
         ball.draw()
         paddle.draw()
     tk.update_idletasks() 
     tk.update() 
     time.sleep(0.01)
 
-root.after(2000, game_over)
 
-def game_over():
-	tk = Tk()
-	canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
-	canvas.pack()
-	canvas.create_text(150, 100, text='game over!!')
-	canvas.create_text(130, 120, text='in 2020.', fill='red')
-	tk.update()
 
-#End Game Screen - show game is over screen
-#create_ text
-#itemconfig (normal and hidden)
 
-tk = Tk()
-canvas = Canvas(tk, width=500, height=400)
-canvas.pack()
-endgame = canvas.create_text(150, 100, text='end game !!',fill='red')
-canvas.itemconfigure(endgame, fill='blue', state='hidden')
-canvas.itemconfigure(endgame, fill='blue', state='normal')
-tk.update()
 
 #Game score - add a game score showing how many times user played and won
-#itemconfig
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+endgame = canvas.create_text(150, 100, text='end game !!',fill='red')
+#canvas.itemconfigure(endgame, fill='blue', state='hidden')
+canvas.pack()
+canvas.itemconfigure(endgame, fill='blue', state='normal')
+tk.mainloop()
+	
 #Ball acceleration - increase the speed of the ball
+
+#End Game Screen - show game is over screen
 
 
 #print(self.canvas.coords(self.id))
