b8ad58abd235c421b52baadb8e290b489d0cdc65
jualshe <shevchenko.j@gmail.com>
2020-12-30 11:00:09 -0800

    Update game.py

--- game.py
+++ game.py
@@ -16,6 +16,7 @@ class Ball:
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
         self.hit_bottom = False
+
     def hit_paddle(self, pos):
         paddle_pos = self.canvas.coords(self.paddle.id)
         if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
@@ -29,6 +30,7 @@ class Ball:
             	self.score.hit()
                 return True
             return False
+
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
@@ -42,6 +44,7 @@ class Ball:
             self.x = 3
         if pos[2] >= self.canvas_width:
             self.x = -3
+
 class Paddle:
     def __init__(self, canvas, color):
         self.canvas = canvas
@@ -89,7 +92,7 @@ tk.update()
 
 score = Score(canvas, 'green')
 paddle = Paddle(canvas, 'blue')
-ball = Ball(canvas,paddle, 'red')
+ball = Ball(canvas,paddle, score, 'red')
 game_over_text = canvas.create_text(250, 200, text='GAME OVER !!', state='hidden')
 
 while 1:
