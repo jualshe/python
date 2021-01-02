daf979a413b17264dbb0dbd7cdbb63b0958de782
jualshe <shevchenko.j@gmail.com>
2021-01-01 22:37:27 -0800

    Update game.py

--- game.py
+++ game.py
@@ -1,80 +1,82 @@
+# -*- coding: utf-8 -*-
 from tkinter import *
 import random
 import time
 
 class Ball:
+
 	def __init__(self, canvas, paddle, score, color):
-        self.canvas = canvas
-        self.paddle = paddle
-        self.score = score
-        self.id = canvas.create_oval(10,10,25,25, fill=color)
-        self.canvas.move(self.id, 245,100)
-        starts = [-3, -2, -1, 1, 2, 3]
-        random.shuffle(starts)
-        self.x = starts[0]
-        self.y = -3
-        self.canvas_height = self.canvas.winfo_height()
-        self.canvas_width = self.canvas.winfo_width()
-        self.hit_bottom = False
+		self.canvas = canvas
+		self.paddle = paddle
+		self.score = score
+		self.id = canvas.create_oval(10,10,25,25, fill=color)
+		self.canvas.move(self.id, 245,100)
+		starts = [-3, -2, -1, 1, 2, 3]
+		random.shuffle(starts)
+		self.x = starts[0]
+		self.y = -3
+		self.canvas_height = self.canvas.winfo_height()
+		self.canvas_width = self.canvas.winfo_width()
+		self.hit_bottom = False
 
-    def hit_paddle(self, pos):
-        paddle_pos = self.canvas.coords(self.paddle.id)
-        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
-            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
-            	#accelerate the ball
-            	#the ball to speed up if it’s travelling in the same horizontal direction when it
-				#hits the paddle, and slow down if it’s going in the opposite horizontal direction. 
-				#In order to do this, the left-right (horizontal) speed of
-				#the paddle should be added to the horizontal speed of the ball
-            	self.x += self.paddle.x
-            	self.score.hit()
-                return True
-            return False
+	def hit_paddle(self, pos):
+		paddle_pos = self.canvas.coords(self.paddle.id)
+		if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+			if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+		#accelerate the ball
+		#the ball to speed up if it’s travelling in the same horizontal direction when it
+		#hits the paddle, and slow down if it’s going in the opposite horizontal direction. 
+		#In order to do this, the left-right (horizontal) speed of
+		#the paddle should be added to the horizontal speed of the ball
+				self.x += self.paddle.x
+				self.score.hit()
+				return True
+			return False
 
-    def draw(self):
-        self.canvas.move(self.id, self.x, self.y)
-        pos = self.canvas.coords(self.id)
-        if pos[1] <= 0:
-            self.y = 3
-        if pos[3] >= self.canvas_height:
-            self.hit_bottom = True
-        if self.hit_paddle(pos) == True:
-            self.y = -3
-        if pos[0] <= 0:
-            self.x = 3
-        if pos[2] >= self.canvas_width:
-            self.x = -3
+	def draw(self):
+		self.canvas.move(self.id, self.x, self.y)
+		pos = self.canvas.coords(self.id)
+		if pos[1] <= 0:
+			self.y = 3
+		if pos[3] >= self.canvas_height:
+			self.hit_bottom = True
+		if self.hit_paddle(pos) == True:
+			self.y = -3
+		if pos[0] <= 0:
+			self.x = 3
+		if pos[2] >= self.canvas_width:
+			self.x = -3
 
 class Paddle:
-    def __init__(self, canvas, color):
-        self.canvas = canvas
-        self.id = canvas.create_rectangle(0,0,100,10, fill=color)
-        self.canvas.move(self.id, 200, 300)
-        self.x = 0
-        self.canvas_width = self.canvas.winfo_width()
-        self.started = False
-        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
-        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
-        self.canvas.bind_all('<Button-1>',self.start_game)
-        
-    def turn_left(self, evt):
-        self.x = -2
-    def turn_right(self, evt):
-        self.x = 2
+	def __init__(self, canvas, color):
+		self.canvas = canvas
+		self.id = canvas.create_rectangle(0,0,100,10, fill=color)
+		self.canvas.move(self.id, 200, 300)
+		self.x = 0
+		self.canvas_width = self.canvas.winfo_width()
+		self.started = False
+		self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+		self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+		self.canvas.bind_all('<Button-1>',self.start_game)
 
-    def start_game(self, evt):
-        self.started = True
-        
-    def draw(self):
-        self.canvas.move(self.id, self.x, 0)
-        pos = self.canvas.coords(self.id)
-        if pos[0] <= 0:
-            self.x = 0
-        elif pos[2] >= self.canvas_width:
-            self.x = 0
+	def turn_left(self, evt):
+		self.x = -2
+	def turn_right(self, evt):
+		self.x = 2
+
+	def start_game(self, evt):
+		self.started = True
+
+	def draw(self):
+		self.canvas.move(self.id, self.x, 0)
+		pos = self.canvas.coords(self.id)
+		if pos[0] <= 0:
+			self.x = 0
+		elif pos[2] >= self.canvas_width:
+			self.x = 0
 
 class Score:
- 	def __init__(self, canvas, color):
+	def __init__(self, canvas, color):
 		self.score = 0
 		self.canvas = canvas
 		self.id = canvas.create_text(450, 10, text=self.score, fill=color)
@@ -90,24 +92,19 @@ canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
-score = Score(canvas, 'green')
+score = Score(canvas, 'purple')
 paddle = Paddle(canvas, 'blue')
-ball = Ball(canvas,paddle, score, 'red')
+ball = Ball(canvas,paddle,score, 'red')
 game_over_text = canvas.create_text(250, 200, text='GAME OVER !!', state='hidden')
 
 while 1:
-    if ball.hit_bottom == False and paddle.started == True:
-        ball.draw()
-        paddle.draw()
-    if ball.hit_bottom == True:
-        time.sleep(1)
-        canvas.itemconfig(game_over_text, fill='red',font=('Helvetica', 30), state='normal')
-        
+	if ball.hit_bottom == False and paddle.started == True:
+		ball.draw()
+		paddle.draw()
+	if ball.hit_bottom == True:
+		time.sleep(1)
+		canvas.itemconfig(game_over_text, fill='red',font=('Helvetica', 20), state='normal')
+
     tk.update_idletasks() 
     tk.update() 
     time.sleep(0.01)
-	
-#Ball acceleration - increase the speed of the ball
-
-#print(self.canvas.coords(self.id))
-#[255.0, 29.0, 270.0, 44.0]
