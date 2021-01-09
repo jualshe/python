f49c84154b1939b523585e466e16557d5b82dffb
jualshe <shevchenko.j@gmail.com>
2021-01-09 00:01:19 -0800

    Update seeking_the_exit_game.py

--- seeking_the_exit_game.py
+++ seeking_the_exit_game.py
@@ -20,5 +20,10 @@ class Game:
 	def __init__(self):
 		self.tk = Tk()
 	self.tk.title("man is seeking the exit") 
-	self.tk.resizable(0, 0) self.tk.wm_attributes("-topmost", 1)
+	self.tk.resizable(0, 0) 
+	self.tk.wm_attributes("-topmost", 1)
 	self.canvas = Canvas(self.tk, width=500, height=500, highlightthickness=0)
+	self.canvas.pack() 
+	self.tk.update() 
+	self.canvas_height = 500 
+	self.canvas_width = 500
