c9a2fe817b59f0c182fa5ca7437d703f4b6aa51b
jualshe <shevchenko.j@gmail.com>
2021-01-14 23:32:37 -0800

    Update stickmangame.py

--- stickman/stickmangame.py
+++ stickman/stickmangame.py
@@ -44,6 +44,13 @@ class Game:
 			self.tk.update_idletasks()
 			self.tk.update()
 			time.sleep(0.01)
-			
+
 g = Game() 
 g.mainloop()
+
+class Coords:
+def __init__(self, x1=0, y1=0, x2=0, y2=0):
+	self.x1 = x1 
+	self.y1 = y1 
+	self.x2 = x2 
+	self.y2 = y2
