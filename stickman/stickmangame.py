b14cc1f8bc15a6c761b4e150dc09bd49f5a5a192
jualshe <shevchenko.j@gmail.com>
2021-02-16 23:28:11 -0800

    Update stickmangame.py

--- stickman/stickmangame.py
+++ stickman/stickmangame.py
@@ -110,3 +110,10 @@ def collided_top(co1, co2):
 		if co1.y1 <= co2.y2 and co1.y1 >= co2.y1:
 			return True
 	return False
+
+def collided_bottom(y, co1, co2):
+    if within_x(co1, co2):
+    	y_calc = co1.y2 + y
+    	if y_calc >= co2.y1 and y_calc <= co2.y2:
+            return True
+    return False
